5ae44b83e11ab0d1747247732cce90e24ef00df3
Paulo Santana <paulo.santana.r@gmail.com>
2022-11-15 18:31:01 -0300

    kkkkkkkkkkkkkkkk asideia

--- back/src/match-making/match-making.service.ts
+++ back/src/match-making/match-making.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { randomUUID } from 'crypto';
-import { UsersService } from 'src/users/users.service';
-import { MatchType, MATCH_TYPES } from './dto/AppendToQueueDTO';
+import {Injectable, Logger} from '@nestjs/common';
+import {randomUUID} from 'crypto';
+import {UsersService} from 'src/users/users.service';
+
+import {MATCH_TYPES, MatchType} from './dto/AppendToQueueDTO';
 
 class MemoryQueue {
   CLASSIC: Express.User[] = [];
@@ -33,7 +34,8 @@ export class MatchMakingService {
 
   constructor(private usersService: UsersService) {}
 
-  // PERF: we could save the queue the user is on in the database for better dequeueing
+  // PERF: we could save the queue the user is on in the database for better
+  // dequeueing
   enqueue(user: Express.User, matchType: MatchType) {
     const queue = this.memoryQueue[matchType];
 
@@ -41,46 +43,39 @@ export class MatchMakingService {
       throw new Error('User is already in a queue');
     }
     this.logger.log(
-      `user ${user.login_intra} is entering the ${matchType} match-making queue`,
+        `user ${user.login_intra} is entering the ${
+            matchType} match-making queue`,
     );
 
     queue.push(user);
 
     if (this.isMatchAvailable(queue)) {
       return this.createMatch(queue);
-    }
-    return null;
-  }
-
-  dequeue(user: Express.User) {
-    MATCH_TYPES.forEach((type) => {
-      const queue = this.memoryQueue[type];
-      const i = queue.findIndex((queuedUser) => (queuedUser.id = user.id));
-      if (i >= 0) {
-        this.logger.debug(
+      this.logger.debug(
           `removing user ${user.login_intra} from the ${type} queue`,
-        );
-        queue.splice(i, 1);
-      }
-    });
-  }
+      );
+      queue.splice(i, 1);
+    }
+  });
+}
 
-  private isMatchAvailable(queue: Express.User[]): boolean {
-    return queue.length >= 2;
-  }
+private isMatchAvailable(queue: Express.User[]): boolean {
+  return queue.length >= 2;
+}
 
-  private createMatch(queue: Express.User[]): MemoryMatch {
-    const [user1, user2] = queue;
-    this.logger.debug(
-      `Creating a match between users ${user1.login_intra} and ${user2.login_intra}`,
-    );
+private createMatch(queue: Express.User[]): MemoryMatch {
+  const [user1, user2] = queue;
+  this.logger.debug(
+      `Creating a match between users ${user1.login_intra} and ${
+          user2.login_intra}`,
+  );
 
-    const match = new MemoryMatch(randomUUID(), user1, user2);
-    this.memoryMatches.push(match);
+  const match = new MemoryMatch(randomUUID(), user1, user2);
+  this.memoryMatches.push(match);
 
-    queue.shift();
-    queue.shift();
+  queue.shift();
+  queue.shift();
 
-    return match;
-  }
+  return match;
+}
 }
